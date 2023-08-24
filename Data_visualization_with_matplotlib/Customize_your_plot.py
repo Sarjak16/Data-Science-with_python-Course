7a7c1162cd4f61eda6c93ec39572a9af57ee2024
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-08-24 20:42:12 +0545

    Update Customize_your_plot.py

--- Data_visualization_with_matplotlib/Customize_your_plot.py
+++ Data_visualization_with_matplotlib/Customize_your_plot.py
@@ -4,3 +4,11 @@
 # Pass the marker key-word arguments to these commands to set the Seattle data to circle markers ('o') and the Austin markers to triangles pointing downwards ('v').
 # Pass the linestyle key-word argument to use dashed lines for the data from both cities ('--').
 
+# Plot Seattle data, setting data appearance
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"], color="b", marker="o", linestyle="--")
+
+# Plot Austin data, setting data appearance
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"], color="r", marker="v", linestyle="--")
+
+# Call show to display the resulting plot
+plt.show()
