99a7cbcfc2315e599162ee3cdb139e62c1118f94
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-08-24 20:50:16 +0545

    Update Customize_your_plot.py

--- Data_visualization_with_matplotlib/Customize_your_plot.py
+++ Data_visualization_with_matplotlib/Customize_your_plot.py
@@ -12,3 +12,23 @@ ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"], color="r", m
 
 # Call show to display the resulting plot
 plt.show()
+
+#Customizing axis labels and adding titles........................................................................................................
+# Use the set_xlabel method to add the label: "Time (months)".
+# Use the set_ylabel method to add the label: "Precipitation (inches)".
+# Use the set_title method to add the title: "Weather patterns in Austin and Seattle".
+
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-PRCP-NORMAL"])
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-PRCP-NORMAL"])
+
+# Customize the x-axis label
+ax.set_xlabel ("Time (months)")
+
+# Customize the y-axis label
+ax.set_ylabel ("Precipitation (inches)")
+
+# Add the title
+ax.set_title("Weather patterns in Austin and Seattle")
+
+# Display the figure
+plt.show()
