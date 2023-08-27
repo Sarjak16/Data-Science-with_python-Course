fa1e29397df1f89a4f41838a945a0af364dbf937
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-08-27 21:17:17 +0545

    Update plotting_time_series_data.py

--- Data_visualization_with_matplotlib/plotting_time_series_data.py
+++ Data_visualization_with_matplotlib/plotting_time_series_data.py
@@ -10,3 +10,24 @@ import pandas as pd
 
 # Read the data from file using read_csv
 climate_change = pd.read_csv('climate_change.csv',parse_dates=["date"],index_col="date")
+
+#Plot time-series data
+# Add the data from climate_change to the plot: use the DataFrame index for the x value and the "relative_temp" column for the y values.
+# Set the x-axis label to 'Time'.
+# Set the y-axis label to 'Relative temperature (Celsius)'.
+# Show the figure.
+
+import matplotlib.pyplot as plt
+fig, ax = plt.subplots()
+
+# Add the time-series for "relative_temp" to the plot
+ax.plot(climate_change.index, climate_change['relative_temp'])
+
+# Set the x-axis label
+ax.set_xlabel('Time')
+
+# Set the y-axis label
+ax.set_ylabel('Relative temperature (Celsius)')
+
+# Show the figure
+plt.show()
