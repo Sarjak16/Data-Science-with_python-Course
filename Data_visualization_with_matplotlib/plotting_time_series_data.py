81b77e35b80d796e87ee1f4586aca5041ca90250
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-08-29 22:18:14 +0545

    Update plotting_time_series_data.py

--- Data_visualization_with_matplotlib/plotting_time_series_data.py
+++ Data_visualization_with_matplotlib/plotting_time_series_data.py
@@ -52,3 +52,30 @@ ax.plot(seventies.index, seventies["co2"])
 
 # Show the figure
 plt.show()
+
+
+
+
+
+# PLOTTING TIMESERIES WITH DIFFERENT VARIABLES............................................................................................
+# 1.Plotting two variables................................
+# Use plt.subplots to create a Figure and Axes objects called fig and ax, respectively.
+# Plot the carbon dioxide variable in blue using the Axes plot method.
+# Use the Axes twinx method to create a twin Axes that shares the x-axis.
+# Plot the relative temperature variable in red on the twin Axes using its plot method.
+
+import matplotlib.pyplot as plt
+
+# Initalize a Figure and Axes
+fig,ax=plt.subplots()
+
+# Plot the CO2 variable in blue
+ax.plot(climate_change.index, climate_change["co2"], color='blue')
+
+# Create a twin Axes that shares the x-axis
+ax2 = ax.twinx()
+
+# Plot the relative temperature in red
+ax2.plot(climate_change.index, climate_change["relative_temp"], color='red')
+
+plt.show()
