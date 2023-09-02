e96bda816a1db9d2e4913feb9fea2220c61aa0f3
Sarjak Kuikel <104067888+Sarjak16@users.noreply.github.com>
2023-09-02 20:57:43 +0545

    Update plotting_time_series_data.py

--- Data_visualization_with_matplotlib/plotting_time_series_data.py
+++ Data_visualization_with_matplotlib/plotting_time_series_data.py
@@ -140,3 +140,12 @@ ax.plot(climate_change.index, climate_change['relative_temp'])
 ax.annotate(">1 degree", xy=(pd.Timestamp('2015-10-06'), 1))
 
 plt.show()
+
+
+
+# Plotting time-series: putting it all together,..........................................................................................
+# Use the plot_timeseries function to plot CO2 levels against time. Set xlabel to "Time (years)" ylabel to "CO2 levels" and color to 'blue'.
+# Create ax2, as a twin of the first Axes.
+# In ax2, plot temperature against time, setting the color ylabel to "Relative temp (Celsius)" and color to 'red'.
+# Annotate the data using the ax2.annotate method. Place the text ">1 degree" in x=pd.Timestamp('2008-10-06'), y=-0.2 pointing with a gray thin arrow to x=pd.Timestamp('2015-10-06'), y = 1.
+
