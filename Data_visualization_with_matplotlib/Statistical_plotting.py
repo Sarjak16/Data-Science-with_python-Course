bfaa65eeacd8ba06490456d9c982ecf55143b654
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-11-06 20:53:45 +0545

    Update Statistical_plotting.py

--- Data_visualization_with_matplotlib/Statistical_plotting.py
+++ Data_visualization_with_matplotlib/Statistical_plotting.py
@@ -35,10 +35,10 @@ plt.show()
 
 
 
-Create a boxplot that contains the "Height" column for mens_rowing on the left and mens_gymnastics on the right.
-Add x-axis tick labels: "Rowing" and "Gymnastics".
-Add a y-axis label: "Height (cm)".
-fig, ax = plt.subplots()
+# Create a boxplot that contains the "Height" column for mens_rowing on the left and mens_gymnastics on the right.
+# Add x-axis tick labels: "Rowing" and "Gymnastics".
+# Add a y-axis label: "Height (cm)".
+# fig, ax = plt.subplots()
 
 # Add a boxplot for the "Height" column in the DataFrames
 ax.boxplot([mens_rowing["Height"], mens_gymnastics["Height"]])
