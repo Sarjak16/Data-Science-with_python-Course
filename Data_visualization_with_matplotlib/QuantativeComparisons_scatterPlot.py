605daf1839b5e99b324e034415fa1b6fc4a9f62c
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-11-24 21:01:11 +0545

    Update QuantativeComparisons_scatterPlot.py

---
+++ Data_visualization_with_matplotlib/QuantativeComparisons_scatterPlot.py
@@ -0,0 +1,4 @@
+#simple scatter plot
+# Using the ax.scatter method, add the data to the plot: "co2" on the x-axis and "relative_temp" on the y-axis.
+# Set the x-axis label to "CO2 (ppm)".
+# Set the y-axis label to "Relative temperature (C)".
