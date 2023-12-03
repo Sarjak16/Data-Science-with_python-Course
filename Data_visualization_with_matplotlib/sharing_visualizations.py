9bb679ef036c5bb598611ec7131e300e56bcc384
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-03 20:08:52 +0545

    Update sharing_visualizations.py

--- Data_visualization_with_matplotlib/sharing_visualizations.py
+++ Data_visualization_with_matplotlib/sharing_visualizations.py
@@ -1,2 +1,7 @@
 #switching between styles:
 #Select the 'ggplot' style, create a new Figure called fig, and a new Axes object called ax with plt.subplots.
+# Use the "ggplot" style and create new Figure/Axes
+plt.style.use("ggplot")
+fig,ax = plt.subplots()
+ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-TAVG-NORMAL"])
+plt.show()
