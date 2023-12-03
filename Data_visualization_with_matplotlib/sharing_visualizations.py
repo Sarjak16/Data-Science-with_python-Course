795f20eb7f7c051e75f5701b7b888f4932e58544
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-03 20:10:15 +0545

    Update sharing_visualizations.py

--- Data_visualization_with_matplotlib/sharing_visualizations.py
+++ Data_visualization_with_matplotlib/sharing_visualizations.py
@@ -5,3 +5,10 @@ plt.style.use("ggplot")
 fig,ax = plt.subplots()
 ax.plot(seattle_weather["MONTH"], seattle_weather["MLY-TAVG-NORMAL"])
 plt.show()
+
+#Select the 'Solarize_Light2' style, create a new Figure called fig, and a new Axes object called ax with plt.subplots.
+# Use the "Solarize_Light2" style and create new Figure/Axes
+plt.style.use('Solarize_Light2')
+fig,ax = plt.subplots()
+ax.plot(austin_weather["MONTH"], austin_weather["MLY-TAVG-NORMAL"])
+plt.show()
