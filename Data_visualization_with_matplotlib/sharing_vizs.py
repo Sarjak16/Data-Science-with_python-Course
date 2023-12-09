262f2526e9c8ddded69288768d187c3a39d721e2
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-09 21:09:58 +0545

    Update sharing_vizs.py

--- Data_visualization_with_matplotlib/sharing_vizs.py
+++ Data_visualization_with_matplotlib/sharing_vizs.py
@@ -1,1 +1,7 @@
 #Sharing_Visualizations
+
+# The data you will use is the same weather data we used in the first lesson: you will have available to you the DataFrame seattle_weather and the DataFrame austin_weather, both with records of the average temperature in every month.
+
+# Select the 'ggplot' style, create a new Figure called fig, and a new Axes object called ax with plt.subplots.
+
+# Select the 'Solarize_Light2' style, create a new Figure called fig, and a new Axes object called ax with plt.subplots.
