d9827f7259a2f2bdc0477dc14a3bca44f06f5b85
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-11 21:34:55 +0545

    Update sharing_vizs.py

--- Data_visualization_with_matplotlib/sharing_vizs.py
+++ Data_visualization_with_matplotlib/sharing_vizs.py
@@ -41,3 +41,13 @@ fig.savefig('figure_3_5.png')
 # Set figure dimensions and save as a PNG
 fig.set_size_inches([5, 3])
 fig.savefig('figure_5_3.png')
+
+
+# Saving file sevral times
+Examine the figure by calling the plt.show() function.
+ 
+Save the figure into the file my_figure.png, using the default resolution.
+
+
+
+Save the figure into the file my_figure_300dpi.png and set the resolution to 300 dpi.
