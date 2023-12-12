c80caf6830b7d084354d89dcba2b7c266c87092f
Sarjak16 <104067888+Sarjak16@users.noreply.github.com>
2023-12-12 20:24:12 +0545

    Update automating_fig_from_datas.py

--- Data_visualization_with_matplotlib/automating_fig_from_datas.py
+++ Data_visualization_with_matplotlib/automating_fig_from_datas.py
@@ -1,3 +1,10 @@
+
+# In this exercise, we will extract the unique values of the "Sport" column
+
+# Create a variable called sports_column that holds the data from the "Sport" column of the DataFrame object.
+# Use the unique method of this variable to find all the unique different sports that are present in this data, and assign these values into a new variable called sports.
+# Print the sports variable to the console.
+
 # Extract the "Sport" column
 sports_column = summer_2016_medals["Sport"]
 
@@ -6,3 +13,4 @@ sports = sports_column.unique()
 
 # Print out the unique sports values
 print(sports)
+
